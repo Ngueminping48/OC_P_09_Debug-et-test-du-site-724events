fa46cae94adab7efe745c2071455c536f37cc5ad
Ngueminping48 <annelaurencekamenikameni@gmail.com>
2024-10-18 19:31:53 +0200

    containers contextspages app.css + colors.css

--- src/containers/Slider/index.js
+++ src/containers/Slider/index.js
@@ -1,57 +1,77 @@
-import { useEffect, useState } from 'react';
-import { useData } from '../../contexts/DataContext';
-import { getMonth } from '../../helpers/Date';
+import { useEffect, useState, useMemo } from "react";
+import { useData } from "../../contexts/DataContext";
+import { getMonth } from "../../helpers/Date";
 
-import './style.scss';
+import "./style.scss";
 
 const Slider = () => {
   const { data } = useData();
   const [index, setIndex] = useState(0);
+  // const [paused, setPaused] = useState(false); // État pour savoir si le slider est en pause
 
-  const byDateDesc =
-    data?.focus?.sort((evtA, evtB) =>
-      new Date(evtA.date) < new Date(evtB.date) ? -1 : 1
-    ) || [];
+  const byDateDesc = useMemo(() => 
+    (data?.focus?.sort((evtA, evtB) =>
+      new Date(evtA.date) > new Date(evtB.date) ? -1 : 1
+    ) || [])
+  , [data?.focus]);
+  
 
   useEffect(() => {
-    const intervalId = setInterval(() => {
+    const interval = setInterval(() => {
+      // if (!paused) { // Vérifie si le slider n'est pas en pause et si il n'y a pas de défilement automatique
       setIndex((prevIndex) =>
         prevIndex < byDateDesc.length - 1 ? prevIndex + 1 : 0
       );
-    }, 3000);
+    
+    }, 5000);
+    return () => clearInterval(interval);
+  }, [{/* paused */} ,byDateDesc]);
 
-    return () => clearInterval(intervalId);
-  }, [byDateDesc.length, index]);
+  //  Gérer l'appui sur la barre d'espace pour mettre le slider en pause
+  // useEffect(() => {
+  // const handleKeyDown = (event) => {
+  // if (event.code === 'Space') { // Vérifie si la barre d'espace est pressée
+  // event.preventDefault(); // Empêche le défilement de la page
+  // setPaused((prevPaused) => !prevPaused); // Inverse l'état de pause
+  // }
+  // };
+
+  // window.addEventListener('keydown', handleKeyDown); // Ajoute l'écouteur d'événements
+
+  // return () => {
+  // window.removeEventListener('keydown', handleKeyDown); // Nettoie l'écouteur d'événements
+  // };
+  // }, []);
 
   const handleRadioChange = (radioIdx) => {
     setIndex(radioIdx);
   };
 
   return (
-    <div className='SlideCardList'>
+    <div className="SlideCardList">
       {byDateDesc.map((event, idx) => (
         <div key={event.id || `${event.title}-${idx}`}>
           <div
             className={`SlideCard SlideCard--${
-              index === idx ? 'display' : 'hide'
+              index === idx ? "display" : "hide"
             }`}
           >
-            <img src={event.cover} alt='forum' />
-            <div className='SlideCard__descriptionContainer'>
-              <div className='SlideCard__description'>
+            <img src={event.cover} alt="forum" />
+            <div className="SlideCard__descriptionContainer">
+              <div className="SlideCard__description">
                 <h3>{event.title}</h3>
                 <p>{event.description}</p>
                 <div>{getMonth(new Date(event.date))}</div>
               </div>
             </div>
           </div>
-          <div className='SlideCard__paginationContainer'>
-            <div className='SlideCard__pagination'>
+          <div className="SlideCard__paginationContainer">
+            <div className="SlideCard__pagination">
               {byDateDesc.map((_, radioIdx) => (
                 <input
                   key={`${event.id || `${event.title}-${radioIdx}`}`}
-                  type='radio'
-                  name='radio-button'
+                  type="radio"
+                  name="radio-button"
                   checked={index === radioIdx}
                   onChange={() => handleRadioChange(radioIdx)}
                 />
